c38e6c97b27940f67321d10305e9a9532bcc7f46
domonic <domonicneal3@yahoo.com>
2019-07-11 13:13:35 -0500

    Update DeepLearning_Lesson3.py

--- DeepLearning_Lesson3.py
+++ DeepLearning_Lesson3.py
@@ -6,13 +6,15 @@ from keras_preprocessing.sequence import pad_sequences
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
+from sklearn.datasets import fetch_20newsgroups
+newsgroups_train =fetch_20newsgroups(subset='train', shuffle=True)
+sentences = newsgroups_train.data
+y = newsgroups_train.target
 
-
-
-df = pd.read_csv('imdb_master.csv',encoding='latin-1')
-print(df.head())
-sentences = df['review'].values
-y = df['label'].values
+#df = pd.read_csv('imdb_master.csv',encoding='latin-1')
+#print(df.head())
+#sentences = df['review'].values
+#y = df['label'].values
 
 max_review_len = max([len(s.split())for s in sentences])
 
@@ -30,20 +32,29 @@ vocab_size = len(tokenizer.word_index) + 1
 
 le = preprocessing.LabelEncoder()
 y = le.fit_transform(y)
-X_train, X_test, y_train, y_test = train_test_split(padded_docs[:10000], y[:10000], test_size=0.25, random_state=1000)
+X_train, X_test, y_train, y_test = train_test_split(padded_docs, y, test_size=0.25, random_state=1000)
+
 
 # Number of features
 #print(input_dim)
 model = Sequential()
 
-'''Do not need input_dim after the first layer'''
+
+'''Code based on given dataset'''
+#model.add(layers.Embedding(vocab_size, 50, input_length=max_review_len))
+#model.add(layers.Flatten())
+#model.add(layers.Dense(300, activation='relu'))
+#model.add(layers.Dense(3, activation='softmax'))
+#model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['acc'])
+#history=model.fit(X_train,y_train, epochs=2, verbose=True, validation_data=(X_test,y_test), batch_size=256)
+
+'''Code based on 20newsgroups'''
 model.add(layers.Embedding(vocab_size, 50, input_length=max_review_len))
 model.add(layers.Flatten())
 model.add(layers.Dense(300, activation='relu'))
-model.add(layers.Dense(3, activation='softmax'))
+model.add(layers.Dense(20, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['acc'])
 history=model.fit(X_train,y_train, epochs=2, verbose=True, validation_data=(X_test,y_test), batch_size=256)
-
 
 [test_loss, test_acc] = model.evaluate(X_test, y_test)
 print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
